1f66e1eca065c6f6ad97f5e9bdacc66776ec448e
sletkov <70477360+sletkov@users.noreply.github.com>
2023-12-02 14:51:51 +0300

    added logger

--- internal/handler/grpc/v1/handler.go
+++ internal/handler/grpc/v1/handler.go
@@ -2,6 +2,8 @@ package v1
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
 	api "urlshortener/api/proto"
 	random "urlshortener/internal/pkg"
 )
@@ -14,30 +16,38 @@ type Storage interface {
 type GRPCServer struct {
 	api.UnimplementedURLShortenerServer
 	Storage Storage
+	Logger  *slog.Logger
 }
 
-func New(storage Storage) *GRPCServer {
+func New(logger *slog.Logger, storage Storage) *GRPCServer {
 	return &GRPCServer{
+		Logger:  logger,
 		Storage: storage,
 	}
 }
 
 func (s *GRPCServer) SaveURL(ctx context.Context, req *api.SaveURLRequest) (*api.SaveURLResponse, error) {
+	s.Logger.InfoContext(ctx, fmt.Sprintf("saving url %s", req.GetUrl()))
 	alias := random.MakeRandomString(10)
 	err := s.Storage.SaveURL(ctx, req.GetUrl(), alias)
 	if err != nil {
+		s.Logger.ErrorContext(ctx, "failed to save url")
 		return nil, err
 	}
+	s.Logger.InfoContext(ctx, "url saved")
 	return &api.SaveURLResponse{
 		Alias: alias,
 	}, nil
 }
 
 func (s *GRPCServer) GetURL(ctx context.Context, req *api.GetURLRequest) (*api.GetURLResponse, error) {
+	s.Logger.InfoContext(ctx, "getting url")
 	url, err := s.Storage.GetURL(ctx, req.GetAlias())
 	if err != nil {
+		s.Logger.ErrorContext(ctx, "failed to get url")
 		return nil, err
 	}
+	s.Logger.InfoContext(ctx, fmt.Sprintf("got url %s", url))
 	return &api.GetURLResponse{
 		Url: url,
 	}, nil
